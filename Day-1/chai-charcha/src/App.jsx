3390b2fecf1980525131011dd72b27a881cb9584
S Satyanarayana Patra <satya77888narayanpatra@gmail.com>
2023-10-15 01:28:06 +0530

    save for day1

---
+++ Day-1/chai-charcha/src/App.jsx
@@ -0,0 +1,12 @@
+function App() {
+  return (
+    <>
+      <h1>
+        chai aur react with vite | Satyanarayan Patra | 2 years experience in
+        software development
+      </h1>
+    </>
+  );
+}
+
+export default App;
